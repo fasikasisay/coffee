bae633936a8b49e71b6d2570a1c52c5dcd94c236
fasikasisay <sfasika007@gmail.com>
2026-06-04 00:08:16 +0300

    update

--- app.js
+++ app.js
@@ -976,7 +976,17 @@ function showToast(message, type = '', duration = 3000) {
 }
 
 
+const slides = document.querySelectorAll('.about-slide');
 
+let currentSlide = 0;
+
+setInterval(() => {
+  slides[currentSlide].classList.remove('active');
+
+  currentSlide = (currentSlide + 1) % slides.length;
+
+  slides[currentSlide].classList.add('active');
+}, 4000);
 
 /* 
    SECTION 9 — INIT
